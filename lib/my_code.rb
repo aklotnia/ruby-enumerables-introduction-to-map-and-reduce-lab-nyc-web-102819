8e31d04ac6305d98e4dc212493a1677ac287ae3f
Aaron Klotnia <aklotnia@gmail.com>
2019-10-09 04:30:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,7 +31,7 @@ def map_to_square(array)
   array
 end
 
-def reduce_to_total(array, starting_point == 0)
+def reduce_to_total(array, starting_point = 0)
   if starting_point != 0
     total = 0
     while starting_point < array.length
